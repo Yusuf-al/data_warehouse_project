101352a49151a44ff2c52b4f5f806032fc657df2
Yusuf-al <56336257+Yusuf-al@users.noreply.github.com>
2025-12-11 20:11:00 +0600

    Delete advanced_DA/test.sql

--- advanced_DA/test.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    age_group,
-    customer_segment,
-    COUNT(customer_number) as total_customer,
-    SUM(total_sales) as total_sales_by_age_group
-    FROM gold.report_customers
-GROUP BY age_group,customer_segment
-
